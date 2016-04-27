3cb2a9e1a0096062cfca84eca4ffcae7357663f6
Hugo Caille <hugo.caille@isen-lille.fr>
2016-04-27 21:02:35 +0200

    JWT debugging messages

--- anonymonkey_authority/api/auth.py
+++ anonymonkey_authority/api/auth.py
@@ -9,16 +9,19 @@ from anonymonkey_authority.schemas import User
 
 def check_jwt():
     if 'Authorization' not in request.headers or request.headers['Authorization'][0:3] != 'JWT':
+        print 'Missing authorization header'
         return False
     token = request.headers['Authorization'][4:]
     key = cryptography.hazmat.primitives.serialization.load_pem_public_key(current_app.config['OPENID_ISSUER_KEY'], backend=default_backend())
     try:
         claims = jwt.decode(token, key, audience=current_app.config['OPENID_CLIENT'], issuer=current_app.config['OPENID_ISSUER_CLAIM'])
         if claims is None:
+            print 'Missing claims'
             return False
         g.id_token = claims
         return True
     except jwt.InvalidTokenError as ex:
+        print 'Invalid token'
         print ex
         return False
 
